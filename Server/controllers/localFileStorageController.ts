e387c3c7d4b94ef99e4db1ec66d44463dca4d205
Pham Nam <phamnam.mta@gmail.com>
2023-05-14 06:50:46 +0700

    feat: improve ocr

--- Server/controllers/localFileStorageController.ts
+++ Server/controllers/localFileStorageController.ts
@@ -3,12 +3,19 @@ import catchAsyncError from "../middlewares/catchAsyncError";
 import { readFile, readdir, writeFile, unlink } from "node:fs/promises";
 import { Image } from "image-js";
 import path from "path";
+import axios from "axios";
 import * as Excel from 'exceljs';
 
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+const sharp = require('sharp');
 const fs = require('fs-extra');
+const FormData = require('form-data');
 const { getMrz, readMrz, parse } = require('mrz-detection')
 
 const dataLocation = "Server/data";
+export const aiUrl = process.env.AI_SITE_URL;
 
 // Get file => /files/:fileName
 export const getFile = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
@@ -66,12 +73,25 @@ async function saveImages(imagePath: any, images: any, out: any) {
     return cropPath;
   }
 
-async function saveOCRLabel(cropImage: any, orgImage: any, ocr: any, fileName: any) {
-    const x1 = cropImage.position[0] / orgImage.width;
-    const x2 = (cropImage.position[0] + cropImage.width) / orgImage.width;
-    const y1 = cropImage.position[1] / orgImage.height;
-    const y2 = (cropImage.position[1] + cropImage.height) / orgImage.height;
-    const boundingBoxes = [ [x1, y1, x2, y1, x2, y2, x1, y2] ]
+async function saveOCRLabel(boxes: any, cropImage: any, orgImage: any, ocr: any, fileName: any) {
+    let boundingBoxes = []
+    if (cropImage) {
+        const x1 = cropImage.position[0] / orgImage.width;
+        const x2 = (cropImage.position[0] + cropImage.width) / orgImage.width;
+        const y1 = cropImage.position[1] / orgImage.height;
+        const y2 = (cropImage.position[1] + cropImage.height) / orgImage.height;
+        boundingBoxes = [ [x1, y1, x2, y1, x2, y2, x1, y2] ]
+    } else {
+        const x1 = boxes[0][0] / orgImage.width;
+        const y1 = boxes[0][1] / orgImage.height;
+        const x2 = boxes[1][0] / orgImage.width;
+        const y2 = boxes[1][1] / orgImage.height;
+        const x3 = boxes[2][0] / orgImage.width;
+        const y3 = boxes[2][1] / orgImage.height;
+        const x4 = boxes[3][0] / orgImage.width;
+        const y4 = boxes[3][1] / orgImage.height;
+        boundingBoxes = [ [x1, y1, x2, y2, x3, y3, x4, y4] ]
+    }
     const labels = {
         "$schema": "https://schema.cognitiveservices.azure.com/formrecognizer/2021-03-01/labels.json",
         "document": fileName,
@@ -99,9 +119,24 @@ export const parseImage = async (files: any) => {
         for (let file of files) {
             console.log(`process ${file.filename}`);
             const imagePath = file.path;
+            const formData = new FormData();
+            formData.append('file', fs.createReadStream(imagePath));
+
+            const angleApi = `${aiUrl}/api/v1/angle`;
+            try {
+                const resp = await axios.post(angleApi, formData, {
+                    headers: formData.getHeaders()
+                });
+                const imgBuffer = await sharp(imagePath).rotate(resp.data.angle).toBuffer();
+                await fs.writeFile(imagePath, imgBuffer)
+                console.log(`rotate: ${resp.data.angle}`);
+              } catch (error) {
+                console.error(error);
+            }
             console.time(imagePath);
             const result = { crop: null};
             const orgImage = await Image.load(imagePath);
+            let detectMrzOk = true;
             try {
                 getMrz(orgImage, {
                 debug: false,
@@ -109,21 +144,37 @@ export const parseImage = async (files: any) => {
               });
             } catch (e) {
               console.error(e);
-              continue;
+              detectMrzOk = false;
             }
             console.timeEnd(imagePath);
-            const cropPath = await saveImages(imagePath, result, dataLocation);
-            // toSave.push([imagePath, result]);
+            let ocrImage = imagePath;
+            if (detectMrzOk) {
+                ocrImage = await saveImages(imagePath, result, dataLocation);
+            }
+
+            const ocrApi = `${aiUrl}/api/v1/text`;
+            const formData2 = new FormData();
+            formData2.append('file', fs.createReadStream(ocrImage));
             try {
-                const ocrized = await readMrz(await Image.load(cropPath));
-                await saveOCRLabel(result.crop, orgImage, ocrized, file.filename);
-                // console.log(ocrized);
-                // const parsed = parse(ocrized);
-                // console.log(parsed);
-              } catch (e) {
-                console.log(e);
+                const resp = await axios.post(ocrApi, formData2, {
+                    headers: formData2.getHeaders()
+                });
+                const { boxes, txts} = resp.data;
+                await saveOCRLabel(boxes, result.crop , orgImage, txts, file.filename);
+              } catch (error) {
+                console.error(error);
                 continue;
             }
+            // try {
+            //     const ocrized = await readMrz(await Image.load(cropPath));
+            //     await saveOCRLabel(result.crop, orgImage, ocrized, file.filename);
+            //     // console.log(ocrized);
+            //     // const parsed = parse(ocrized);
+            //     // console.log(parsed);
+            //   } catch (e) {
+            //     console.log(e);
+            //     continue;
+            // }
         }
         const fields = {
             "$schema": "https://schema.cognitiveservices.azure.com/formrecognizer/2021-03-01/fields.json",
@@ -198,29 +249,41 @@ function removeDirectory(directory: string) {
     }
   }
 
+function formatDate(inputDate: string) {
+    if (!inputDate || isNaN(Number(inputDate))) {
+        return inputDate;
+    }
+    const year = Number(inputDate.slice(0, 2));
+    const month = Number(inputDate.slice(2, 4)); // Month is zero-based
+    const day = Number(inputDate.slice(4, 6));
+    const date = new Date(year, month, day);
+    return date.toLocaleDateString('en-GB'); // Format as "dd/mm/yyyy"
+}
+
 
 export const exportData = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const fileLabels = fromDir(dataLocation, ".labels.json")
+        												
         const columns = [
-            "filename",
-            "documentCode",
-            "issuingState",
-            "lastName",
-            "firstName",
-            "documentNumber",
-            "documentNumberCheckDigit",
-            "nationality",
-            "birthDate",
-            "birthDateCheckDigit",
-            "sex",
-            "expirationDate",
-            "expirationDateCheckDigit",
-            "personalNumber",
-            "personalNumberCheckDigit",
-            "compositeCheckDigit",
+            "STT",
+            "File",
+            "Họ và tên (*)",
+            "Ngày, tháng, năm sinh (*)",
+            "Giới tính (*)",
+            "Quốc tịch hiện nay (*)",
+            "Quốc tịch gốc",
+            "Nghề nghiệp (*)",
+            "Nơi làm việc",
+            "Số hộ chiếu (*)",
+            "Loại hộ chiếu (*)",
+            "Mục đích nhập cảnh (*)",
+            "Đề nghị từ ngày (*)",
+            "Đến ngày (*)",
+            "Giá trị thị thực (*)",
+            "Nơi nhận thị thực (*)"
           ]
-        const sheetName = "result";
+        const sheetName = "INF_NHAN_SU";
         const exportFile = `${dataLocation}/ocr_result.xlsx`;
         const workbook = await createReportTemplate(
             exportFile,
@@ -229,28 +292,60 @@ export const exportData = catchAsyncError(async (req: Request, res: Response, ne
         );
         const worksheet = workbook.getWorksheet(sheetName);
         let filename = ""
+        let count = 0
         fileLabels.forEach((_path) => {
             try {
                 const rawData = fs.readFileSync(_path);
                 const labels = JSON.parse(rawData);
                 filename = labels.document
-                const ocrized = labels.labels[0]?.value[0]?.ocr || "";
-                if (ocrized && ocrized[1].length === 43) {
-                    ocrized[1] += "0";
+                count += 1
+                let ocrized = labels.labels[0]?.value[0]?.ocr || "";
+                if (ocrized) {
+                    ocrized[0] = ocrized[0].slice(0, 44)
+                    ocrized[1] = ocrized[1].slice(0, 44)
+                    const padding = Math.abs(ocrized[1].length - ocrized[0].length)
+                    if (padding > 0) {
+                        if (ocrized[1].length > ocrized[0].length) {
+                            const textPadding = '<'.repeat(padding);
+                            ocrized[0] += textPadding;
+                        }
+                        else {
+                            const textPadding = '0'.repeat(padding);
+                            ocrized[1] += textPadding;
+                        }
+                    }
+                    const parsed = parse(ocrized);
+                    const state = parsed.fields.nationality == "TWN" ? "Taiwan" : "China";
+                    const record = {
+                        "STT": count,
+                        "File": filename,
+                        "Họ và tên (*)": parsed.fields.firstName + " " + parsed.fields.lastName,
+                        "Ngày, tháng, năm sinh (*)": formatDate(parsed.fields.birthDate),
+                        "Giới tính (*)": parsed.fields.sex == "male" ? "Nam" : "Nữ",
+                        "Quốc tịch hiện nay (*)": state,
+                        "Quốc tịch gốc": state,
+                        "Nghề nghiệp (*)": "",
+                        "Nơi làm việc": "",
+                        "Số hộ chiếu (*)": parsed.fields.documentNumber,
+                        "Loại hộ chiếu (*)": "",
+                        "Mục đích nhập cảnh (*)": "",
+                        "Đề nghị từ ngày (*)": "",
+                        "Đến ngày (*)": "",
+                        "Giá trị thị thực (*)": "",
+                        "Nơi nhận thị thực (*)": "",
+                    }
+                    const row = [
+                        ...Object.values(record).map((value) => value)
+                    ]
+                    worksheet.addRow(row).commit();
+
                 }
-                if (ocrized && ocrized[0].length === 43) {
-                    ocrized[1] += "<";
-                }
-                const parsed = parse(ocrized);
-                const row = [
-                    filename,
-                    ...Object.values(parsed.fields).map((value) => value)
-                ]
-                worksheet.addRow(row).commit();
                 // console.log(parsed);
               } catch (e) {
                 console.log(e);
-                const row = [filename, ...(new Array(columns.length - 1).fill(null))]
+                const row = [...(new Array(columns.length - 1).fill(null))]
+                row[0] = count;
+                row[1] = filename;
                 worksheet.addRow(row).commit();
             }
         })
